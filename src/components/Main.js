25391ad68ddad60049fc16f9afb66f84330c9dbe
stepan litvinov <stepan_litvinov@epam.com>
2019-05-20 13:01:15 +0300

    prepare fore lang

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import '../styles/global.scss'
 import 'animate.css'
-import {Card, Tabs} from "antd";
+import {Card, Dropdown, Icon, Menu, Tabs} from "antd";
 import Login from "./Login";
 import Registration from "./Registration";
 import RestorePasswordInit from "./RestorePasswordInit";
@@ -36,12 +36,32 @@ class Main extends Component {
         return this.state.activeKey.charAt(0).toUpperCase() + this.state.activeKey.slice(1);
     };
 
+    langMenu = (
+        <Menu selectable={true}>
+            <Menu.Item key="0">
+                <a >English</a>
+            </Menu.Item>
+            <Menu.Item key="1">
+                <a >Русский</a>
+            </Menu.Item>
+        </Menu>
+    );
+
+    langDropDown = (
+        <Dropdown overlay={this.langMenu} trigger={['click']}>
+            <a className="ant-dropdown-link" href="#">
+                Lang <Icon type="down" />
+            </a>
+        </Dropdown>
+    );
+
     render() {
         return <div className={'form_container'}>
-            <Card title={this.getTitle()} bordered={false} style={{width: 350}}>
+            <Card title={this.getTitle()} bordered={false} extra={this.langDropDown}>
                 <Tabs tabPosition={"bottom"}
                       activeKey={this.state.activeKey}
-                      onChange={this.onTabChange}>
+                      onChange={this.onTabChange}
+                size={"small"}>
                     <Tabs.TabPane tab="Login" key="login">
                         <Login/>
                     </Tabs.TabPane>
